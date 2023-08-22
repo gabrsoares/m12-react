85dd69ad7cbcdbd593b9fd9e5f660b4c33e37099
Gabriel Soares <gabrsoares@outlook.com>
2023-08-22 17:36:58 -0300

    copy m10 base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Form from './Components/Form/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      marcas: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas', {
+      method:'GET'
+    })
+    .then(response => response.json())
+    .then(data => this.setState({marcas: data}))
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return(
+      <div className='App'>
+        <Form marcas={this.state.marcas}/>
+      </div>
+    )
+  }
 }
 
 export default App;
